eb927a5021d23e01343385234a80fb47cff0eb4a
Borja <borja@borja.es>
2023-01-08 19:49:26 +0100

    act2

--- script1.sh
+++ script1.sh
@@ -7,7 +7,7 @@ sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -t
 sudo vnx -f vnx/nfv3_server_lxc_ubuntu64.xml -t
 
 
-#Destruir los escenearios VNX ---------era -P?
+#Destruir los escenearios VNX
 #cd /home/upm/shared/nfv-lab
 #sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -P
 #sudo vnx -f vnx/nfv3_server_lxc_ubuntu64.xml -P
